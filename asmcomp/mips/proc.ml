2fe203d7e88cfd74f1f1108c067c438a03cbdd97
Chris Dodd <dodd@csl.sri.com>
2014-05-10 19:16:19 -0700

    Fix mips support for latest ocaml changes

--- asmcomp/mips/proc.ml
+++ asmcomp/mips/proc.ml
@@ -195,6 +195,15 @@ let max_register_pressure = function
     Iextcall(_, _) -> [| 6; 6 |]
   | _ -> [| 20; 31 |]
 
+(* Pure operations (without any side effect besides updating their result
+   registers). *)
+
+let op_is_pure = function
+  | Icall_ind | Icall_imm _ | Itailcall_ind | Itailcall_imm _
+  | Iextcall _ | Istackoffset _ | Istore _ | Ialloc _
+  | Iintop(Icheckbound) | Iintop_imm(Icheckbound, _) -> false
+  | _ -> true
+
 (* Layout of the stack *)
 
 let num_stack_slots = [| 0; 0 |]
